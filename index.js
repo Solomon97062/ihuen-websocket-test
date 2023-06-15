0f0b8c73828fbd37f7f48cd28d72d99d7448b6c6
unknown <solomonnarh97062@gmail.com>
2023-06-15 21:35:33 +0000

    what:: server created

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 
 import express from "express";
+import { WebSocketServer } from 'ws';
 
-const app = express();
 
-app.get('/',(req,res)=>{
+const app = express().use(express.json());
+const server = app.listen(8000);
+
+
+app.get('/', (req, res) => {
+    if (req.url == '/api') {
+        const wss = new WebSocketServer({ server });
+        wss.on('connection', (ws, req) => {
+            console.log('Incoming connection: %s', req.url);
+            ws.send(JSON.stringify([{ "message": "Good connet!" }]))
+        });
+    }//0242432092:0530635526
     res.send("Basic Examp...");
+
 });
 
-const server = app.listen(8000);
