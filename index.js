89f2a7e10489ea2134efebad3fa2cf77b67d227c
unknown <solomonnarh97062@gmail.com>
2023-06-22 00:14:19 +0000

    what:: server created

--- index.js
+++ index.js
@@ -1,62 +1,12 @@
-import { WebSocketServer } from 'ws';
-import url from 'url';
 
-const wss = new WebSocketServer({ port: 8080 });
-const ihDB = {
-    "buddies": [{
-        "userid": "xrsolomon",
-        "bType": "single",
-        "bio": "comp.",
-        "name": "Paa Solo",
-        "imageUrl": "https://image.com",
-        "state": "color",
-        "seenWhen": "yesterday",
-        "favDate": "2671:T8y28",
-        "bkList": ["bkList"],
-        "country": "Ghana",
-    }], "shorts": []
-};
-const token = "419";
-const print = (v) => console.log(v);
-const toJson = (v)=> JSON.stringify(v);
+import express from "express";
 
-wss.on('connection', (ws, req) => {
-    const uq = url.parse(req.url, true).query;
-    const r = uq.r;
-    const veri = uq.k == token;
-    print(req.url);
-    if (veri) {
-        var db = ihDB[uq.t]
-        var rs = toJson(db);
-        if (uq.u != '') {
-            ws.send(rs);
-        }
+const app = express();
 
-        ws.on('message', function incoming(data) {
-            var d = JSON.parse(data);
-            if(d['r'] == "set"){
-                const dq = d['q'];
-                db.unshift(dq)
-                ws.send(toJson(db))
-            }
-            if (d['r'] == 'update') {
-                const dq = d['q'];
-                for (const x in db) {
-                    if (Object.hasOwnProperty.call(db, x)) {
-                        for (const y in dq) {
-                            if (Object.hasOwnProperty.call(dq, y)) {
-                                if (uq.u == db[x]["userid"]) {
-                                    if (db[x][y] == dq[y]) continue;
-                                    db[x][y] = dq[y];
-                                }
-                            }
-                        }
-                    }
-                }
-                ws.send(toJson(db))
-            }
-            console.log('received: %s', data);
-        });
-        // ws.send('sucessfully connected to ihuen websocket');
-    }
+
+
+app.get('/',(req,res)=>{
+  res.send("Hello");
 });
+
+app.listen(3000,()=>{console.log('listening on port 3000')}) 
