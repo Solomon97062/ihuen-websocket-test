7ecba556c65d9810f3b7841fb6631606a7eda890
unknown <solomonnarh97062@gmail.com>
2023-06-22 00:44:47 +0000

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 import express from "express";
 import { WebSocketServer } from "ws";
 
-const app = express();
+const app = express().use(express.json());
 
-const server = app.listen(3000,'0.0.0.0',()=>{console.log('listening on port 3000')}) 
+const server = app.listen(3000,()=>{console.log('listening on port 3000')}) 
 
 
 
